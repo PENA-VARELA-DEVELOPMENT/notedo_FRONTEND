41bf4cac77969dd86fa4dbf9b96f831812426b08
Maynor Peña <38294810+PENA98@users.noreply.github.com>
2019-12-01 22:14:41 -0600

    Estructura de vue funcional

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const router = require("./routes/index");
 const path = require("path");
-
+const bodyParser = require('body-parser')
+const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 // settings
 
 // Middlewares
 
-// Routes
-app.use(require)
-// Static Files
 
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+// Static Files
 app.use(express.static(path.join(__dirname,"./public")));
 
+// Routes
+app.use(router())
 
 // Server listenting 
 var server = app.listen(9888, function () {
-    console.log('Node server is running..');
+    console.log('Node server is running on port 9888..');
 });
