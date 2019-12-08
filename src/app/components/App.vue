72123bae6c723d54f79e2bdccb48ce250798cc93
jackeline varela vasquez <jackelinevarela2@gmail.com>
2019-12-08 14:06:00 -0600

    working in design

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,10 +1,33 @@
 <template>
     <div>
-        <nav class="navbar navbar-dark bg-dark">
-        <a href="/" class="navbar-brand">Notedo</a>
-    </nav>
+        <!-- header--->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class=" navbar-brand col-2 " href="/" >Note-do</a>
+            
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           
+            <div class="collapse navbar-collapse" id="navbarResponsive" >
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Crear Lista</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="#">
+                            <i class="fas fa-tasks"></i>
+                                
+                            <span class="count-symbol bg-warning"></span>
+                        </a>             
+                    </li>
+                    <li class="nav-item">fasf</li>
+                </ul>
+            </div>
+        </nav>
     <div class="container">
         
     </div>
+
+    
     </div>
 </template>
