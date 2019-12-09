029efa2a85008effab1ad912093cad192e89a72e
Maynor Peña <38294810+PENA98@users.noreply.github.com>
2019-12-08 20:15:19 -0600

    Components

---
+++ src/app/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <notedo-nv> </notedo-nv>
+
+    <!-- <b-container>
+      <b-row>
+        <b-col>
+          <note-card> </note-card>
+        </b-col>
+        <b-col>
+          <note-card> </note-card>
+        </b-col>
+        <b-col>
+          <note-card> </note-card>
+        </b-col>
+      </b-row>
+    </b-container> -->
+  </div>
+</template>
--- src/app/main.js
+++ src/app/main.js
@@ -1,10 +1,27 @@
+// importar Vue
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter)
+
+// importar Bootstrap para Vue
 import BootstrapVue from 'bootstrap-vue';
-import App from './components/App.vue';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 Vue.use(BootstrapVue);
+
+// importar axios y VueAxios
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
+
+// importar componentes de Vue
+import App from './App.vue';
+import navVar from './components/navVar.vue'
+import card from './components/note.vue';
+
+Vue.component('note-card', card);
+Vue.component('notedo-nv', navVar)
+
 
 new Vue({
     render: h => h(App)
